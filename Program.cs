a75fb3cd4b7a8d07aac7abbce104aea9f59b12fe
Kima4Tec <150744968+Kima4Tec@users.noreply.github.com>
2024-06-03 12:50:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+namespace SeriLogging
+{
+    internal class Program
+    {
+            public static async Task Main(string[] args)
+            {
+                var host = CreateHostBuilder(args).Build();
+
+                // Resolve MyService directly from the host's service provider
+                var myService = host.Services.GetRequiredService<MyService>();
+
+                await myService.DoSomething();
+
+                await myService.StartAsync(CancellationToken.None); // Start the service manually
+
+                await host.RunAsync(); // Run the host           
+            }
+
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                    .ConfigureServices((_, services) =>
+                    {
+                        // Register MyService
+                        services.AddTransient<MyService>();
+                        services.AddSerilog();
+                        // Register other services if needed
+                    }).UseSerilog((context, config) =>
+                    {
+                        config.WriteTo.Console().AuditTo.File("c://dev//kmlog.txt");
+                    });
+        }
+}
